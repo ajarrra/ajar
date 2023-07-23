4eb0ad2fc4fceffb3290317e247ea431f6dddbb8
ajarrra <ajarka1334@gmail.com>
2023-07-23 15:47:03 +0600

    fixed error

--- src/shared/api/api-client.js
+++ src/shared/api/api-client.js
@@ -2,7 +2,7 @@ import { generalSettings } from '../constants'
 
 export const API = {
     get: async (url, param) => {
-        const res = await fetch(`${generalSettings.api}/${url}${param && `/${param}`}`)
+        const res = await fetch(`${generalSettings.api}/${url}`)
 
         return res
     } 
