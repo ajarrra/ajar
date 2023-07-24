5357d2058c171712e64f95d715348f1ca3a3f329
ajarrra <ajarka1334@gmail.com>
2023-07-24 23:37:10 +0600

    fixed

--- src/shared/api/api-client.js
+++ src/shared/api/api-client.js
@@ -3,7 +3,7 @@ import { generalSettings } from '../constants'
 export const API = {
     get: async (url, param) => {
         const res = await fetch(`${generalSettings.api}/${url}`)
-
+        
         return res
     } 
 }
